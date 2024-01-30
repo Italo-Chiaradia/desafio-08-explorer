f4bdc87d53fa388d7b8f649ae1681bce41ac76fb
Italo Chiaradia <102377060+Italo-Chiaradia@users.noreply.github.com>
2024-01-30 16:49:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+// Exercício 1
+/* alert('Hello, world!'); */
+
+// Exercício 2
+/*  
+let x = 10;
+let y = 20;
+console.log(x + y);
+*/
+
+// Exercício 3
+/* let x = 124;
+if (typeof x == 'number') {
+  console.log("É um número");
+} else {
+  console.log("Não é um número");
+} */
+
+// Exercício 4
+/* let x = 'aspodnfao';
+if (typeof x == 'string') {
+  console.log("É uma string");
+} else {
+  console.log("Não é uma string");
+}  */
+
+// Exercício 5
+/* let x = true;
+if (typeof x == 'boolean') {
+  console.log("É um booleano");
+} else {
+  console.log("Não é um booleano");
+}  */
+
+// Exercício 6
+/* let x = 10;
+let y = 5;
+console.log(x - y); */
+
+// Exercício 7
+/* let x = 10;
+let y = 5;
+console.log(x * y); */
+
+// Exercício 8
+/* let x = 10;
+let y = 5;
+console.log(x / y); */
+
+// Exercício 9
+/* let x = 12;
+if (x % 2 == 0) {
+  console.log("É um número par");
+} else {
+  console.log("Não é um número par");
+} */
+
+// Exercício 10
+/* let x = 12;
+if (x % 2 != 0) {
+  console.log("É um número ímpar");
+} else {
+  console.log("Não é um número ímpar");
+}  */
